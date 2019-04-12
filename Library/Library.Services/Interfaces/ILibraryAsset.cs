cf086dfb84057eb3b9d78445b5a30d97747d5b5d
Luchezar <Luchezar.d.dimitrov@gmail.com>
2019-04-13 02:03:00 +0300

    Added entities and first service

---
+++ Library/Library.Services/Interfaces/ILibraryAsset.cs
@@ -0,0 +1,19 @@
+﻿using Library.Data.Models;
+using System.Collections.Generic;
+
+namespace Library.Services.Interfaces
+{
+    public interface ILibraryAsset
+    {
+        IEnumerable<LibraryAsset> GetAll();
+        LibraryAsset GetById(int id);
+        void Add(LibraryAsset newAsset);
+        string GetAuthorOrDirector(int id);
+        string GetDeweyIndex(int id);
+        string GetType(int id);
+        string GetTitle(int id);
+        string GetIsbn(int id);
+
+        LibraryBranch GetCurrentLocation(int id);
+    }
+}
---
+++ Library/Library.Services/LibraryAssetService.cs
@@ -0,0 +1,77 @@
+﻿using Library.Data.Models;
+using Library.Services.Interfaces;
+using LibraryData;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Library.Services
+{
+    public class LibraryAssetService : ILibraryAsset
+    {
+        private LibraryContext _context;
+
+        public LibraryAssetService(LibraryContext context)
+        {
+            this._context = context;
+        }
+
+        public void Add(LibraryAsset newAsset)
+        {
+            _context.Add(newAsset);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<LibraryAsset> GetAll()
+        {
+            return _context.LibraryAssets
+                .Include(asset => asset.Status)
+                .Include(asset => asset.Location);
+        }
+
+        public string GetAuthorOrDirector(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LibraryAsset GetById(int id)
+        {
+            return GetAll()
+                //returns a single instantion of an asset
+                .FirstOrDefault(asset => asset.Id == id);
+        }
+
+        public LibraryBranch GetCurrentLocation(int id)
+        {
+            return GetById(id).Location;
+        }
+
+        public string GetDeweyIndex(int id)
+        {
+            if (_context.Books.Any(book => book.Id == id))
+            {
+                return _context.Books.FirstOrDefault(book => book.Id == id).DeweyIndex;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public string GetIsbn(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTitle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetType(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
